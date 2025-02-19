9bf2cde4062eb2df4a424a5b305aba84d9305eb2
willbhedges <willbhedges@icloud.com>
2025-02-18 22:05:35 -0600

    Attempting to Create a Basic Tileset, but can't get the collision yet.

---
+++ viking/Game/game.gd
@@ -0,0 +1,13 @@
+extends Node2D
+@export var player: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var new_player = player.instantiate()
+	add_child(new_player)
+	new_player.set_position(Vector2(100, 200))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
