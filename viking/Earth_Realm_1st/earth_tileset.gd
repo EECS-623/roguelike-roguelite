79830ffb2b97655ff825a619016617094b0c0615
willbhedges <willbhedges@icloud.com>
2025-02-23 13:20:55 -0600

    Added collision to water

--- viking/Earth_Realm_1st/earth_tileset.gd
+++ viking/Earth_Realm_1st/earth_tileset.gd
@@ -12,23 +12,26 @@ func _ready() -> void:
 	
 	
 	altitude.seed = randi()
-	altitude.frequency = 0.007
-	for i in range(width):
-		for j in range(height):
+	altitude.frequency = 0.008
+	for i in range(50):
+		for j in range(50):
+			set_cell(Vector2i(int(0.0-(width/2.0)+i), int(0.0-(height/2.0)+j)),0,Vector2i(0,0))
+	for i in range(50,width):
+		for j in range(50,height):
 			var alt = altitude.get_noise_2d(0.0-(width/ 2.0)+i, 0.0-(height/2.0)+j)
-			
-			if alt < 0:
+
+			if alt > 0:
 				set_cell(Vector2i(int(0.0-(width/2.0)+i), int(0.0-(height/2.0)+j)),0,Vector2i(0,0))
 				var num = randi_range(0,10000)
 				if num < 3:
 					var my_enemy = enemy.instantiate()
-					my_enemy.position = Vector2(0-(width/2.0)+i*4, 0-(height/2.0)+j*4)
+					my_enemy.position = Vector2(0-(width/2.0)+i*6, 0-(height/2.0)+j*6)
 					get_parent().add_child(my_enemy)
 					Global.earth_enemies_left += 1
 					#print("enemy_added")
 	
-					
-					
+			#elif alt < 0 and alt > -.05:
+				#set_cell(Vector2i(int(0.0-(width/2.0)+i), int(0.0-(height/2.0)+j)),1,Vector2i(0,0))
 			else:
 				set_cell(Vector2i(int(0-(width/2.0)+i), int(0-(height/2.0)+j)),0,Vector2i(1,0))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
