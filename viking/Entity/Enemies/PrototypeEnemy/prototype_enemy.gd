690f049985657b7ad823e1fea89c6eba341af533
Andysword <andrewhuang2019@gmail.com>
2025-03-28 22:53:10 -0500

    Created prototype enemy

--- viking/Entity/Enemies/PrototypeEnemy/prototype_enemy.gd
+++ viking/Entity/Enemies/PrototypeEnemy/prototype_enemy.gd
@@ -1,15 +1,10 @@
-extends CharacterBody2D
+extends Entity
 
-class_name Entity
-
-var health: float
-var attack: float
-var projectile_speed: float
-var cast_cooldown: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
