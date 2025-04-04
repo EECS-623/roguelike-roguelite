383a868bc93115e4288575a3d5b4a48a1017409e
Chase Horner <chase.horner@ku.edu>
2025-04-03 23:26:07 -0500

    venom attack

--- viking/Entities/Bosses/Jormungandr/snake.gd
+++ viking/Entities/Bosses/Jormungandr/snake.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 @export var s_head: PackedScene
-@export var s_head2: PackedScene
 @export var s_body: PackedScene
 @export var s_weak_body: PackedScene
 @export var s_tail: PackedScene
+@export var venom: PackedScene
 
 @export var move_timer: Timer
-@export var venom_timer: Timer
 @onready var hp : HealthComponent = $HealthComponent
 
 var snake: Array = []
@@ -31,17 +30,10 @@ var can_spit = false
 
 
 func _ready() -> void:
-	# Connect the move timer signal
-	spawn_snake()
-	
-	for part in snake:
-		#if part.name == "SnakeHead" or part.name == "SnakeBodyWeak":
-		if part.name == "SnakeBodyWeak":
-			for snake_dir in part.get_children():
-				var hurtbox = snake_dir.get_node_or_null("Hurtbox")
-				if hurtbox:
-					hurtbox.health_component = hp
 
+	spawn_snake()
+	await get_tree().create_timer(5.0).timeout
+	can_spit = true
 	
 func _snake_move():
 	if can_move:
@@ -56,7 +48,7 @@ func _snake_move():
 		snake[0].position += direction * segment_size
 		# Update head direction
 		snake[0].set_meta("direction", direction)
-		update_direction2(snake[0], direction, true)
+		update_direction(snake[0], direction)
 		z_indexing()
 		
 	if (hp.current_health <= (hp.max_health / 2)):
@@ -64,55 +56,25 @@ func _snake_move():
 	print(hp.current_health)
 
 func update_direction(segment, new_direction):
-	# Get the active direction name
-	var active_dir = direction_map.get(new_direction, null)
-	# Hide all direction sprites and track the active one
-	var active_sprite = null
-	for dir_name in direction_map.values():
-		var sprite = segment.get_node_or_null(dir_name)
-		if sprite:
-			sprite.visible = (dir_name == active_dir)
-			if dir_name == active_dir:
-				active_sprite = sprite
-
-	# Enable only the collision shapes of the active direction
-	if active_sprite:
-		var hurtbox = active_sprite.get_node_or_null('Hurtbox/CollisionShape2D')
-		var hitbox = active_sprite.get_node_or_null('Hitbox/CollisionShape2D')
-		if hurtbox: hurtbox.set_deferred("disabled", false)
-		if hitbox: hitbox.set_deferred("disabled", false)
-
-	# Disable collision shapes for all non-active sprites
-	for dir_name in direction_map.values():
-		if dir_name != active_dir:
-			var sprite = segment.get_node_or_null(dir_name)
-			if sprite:
-				var hurtbox = sprite.get_node_or_null('Hurtbox/CollisionShape2D')
-				var hitbox = sprite.get_node_or_null('Hitbox/CollisionShape2D')
-				if hurtbox: hurtbox.set_deferred("disabled", true)
-				if hitbox: hitbox.set_deferred("disabled", true)
-
-func update_direction2(segment, new_direction, is_head=false):
-	if is_head:
-		var dir_str = direction_map.get(direction, "")
-		snake[0].get_node("AnimatedSprite2D").play(dir_str)
+	var dir_str = direction_map.get(new_direction, "")
+	segment.get_node("AnimatedSprite2D").play(dir_str)
 
 
 func spawn_snake():
 	var pos = start_pos
 
 	# Spawn head
-	var head = s_head2.instantiate()
+	var head = s_head.instantiate()
 	head.position = pos
 	head.z_index = snake_size
 	add_child(head)
 	snake.append(head)
 
 	# Set initial direction for head
-	head.set_meta("direction", Vector2.LEFT)  # Set initial direction of head
-	update_direction2(head, Vector2.LEFT, true)  # Update head's sprite based on its direction
+	head.set_meta("direction", Vector2.LEFT) # Set initial direction of head
+	update_direction(head, Vector2.LEFT)  # Update head's sprite based on its direction
 	head.get_node("Hurtbox").health_component = hp
-	head.get_node("Hitbox").set_deferred("disabled", true)
+	head.get_node("Hitbox/CollisionShape2D").set_deferred("disabled", true)
 
 	pos.x += segment_size  # Move position for next segment
 
@@ -123,6 +85,10 @@ func spawn_snake():
 		body_part.z_index = snake_size-1-i
 		add_child(body_part)
 		snake.append(body_part)
+		
+		if (i+1) % 3 == 0:
+			body_part.get_node("Hurtbox").health_component = hp
+
 		
 		body_part.set_meta("direction", Vector2.LEFT)  # Set initial direction of head
 		update_direction(body_part, Vector2.LEFT)
@@ -141,6 +107,9 @@ func spawn_snake():
 	update_direction(tail, Vector2.LEFT)  # Update tail's sprite based on its direction
 
 func chase_player(player_position: Vector2):
+	if not can_move:
+		return
+		
 	var direction_to_player = player_position - snake[0].global_position
 	
 	# If the snake is very close, stop moving
@@ -148,7 +117,7 @@ func chase_player(player_position: Vector2):
 		bite_attack()
 	
 	if direction_to_player.length() >= 300 and can_spit:
-		spit_attack()
+		spit_attack(player_position)
 
 	var rand_dir = randi() % 16
 	if rand_dir == 0:
@@ -178,7 +147,6 @@ func bite_attack():
 	can_move = false
 	can_bite = false
 	var dir_str = direction_map.get(direction, "")
-	print("BITE")
 	dir_str += '_Bite'
 	var anim_player = snake[0].get_node("AnimatedSprite2D/AnimationPlayer")
 	anim_player.play(dir_str)
@@ -186,17 +154,24 @@ func bite_attack():
 	can_move = true
 	can_bite = true
 
-func spit_attack():
+func spit_attack(player_position):
 	can_move = false
 	can_spit = false
-	print("SPIT")
-	await get_tree().create_timer(2.0).timeout
+	var dir_str = direction_map.get(direction, "")
+	var bite_str = dir_str + '_Bite'
+	snake[0].get_node("AnimatedSprite2D").play(bite_str)
+	await get_tree().create_timer(.5).timeout
+	var venom_shot = venom.instantiate()
+	venom_shot.global_position = snake[0].global_position
+	get_tree().current_scene.add_child(venom_shot)
+	venom_shot.shoot(player_position)
+	await get_tree().create_timer(.5).timeout
+	snake[0].get_node("AnimatedSprite2D").play(dir_str)
+	await get_tree().create_timer(1.0).timeout
 	can_move = true
 	await get_tree().create_timer(5.0).timeout
 	can_spit = true
 
-func _on_venom_timer_timeout()-> void:
-	can_spit = true
 	
 func enter_rage_mode():
 	#for segment in snake:
@@ -207,11 +182,17 @@ func enter_rage_mode():
 func _on_health_component_death() -> void:
 	#Play death animation
 	can_move = false
+	can_bite = false
+	can_spit = false
+	for i in range(10):
+		for segment in snake:
+			segment.modulate = Color.RED
+			await get_tree().create_timer(0.01).timeout
+			segment.modulate = Color.WHITE
 	#Dialouge
 	#Separate individual body parts 
 	#Make them disapper
 	queue_free()
-
 
 func _on_health_component_t_damage(amount: float) -> void:
 	for segment in snake:
---
+++ viking/Entities/Bosses/Jormungandr/venom.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+@export var speed: float = 500.0
+@export var puddle_duration: float = 5.0
+
+var target_position: Vector2
+var moving = true
+var total_distance
+
+func _process(delta):
+	if moving:
+		var direction = (target_position - global_position).normalized()
+		var distance = speed * delta
+		var distance_to_target = global_position.distance_to(target_position)
+
+		if distance_to_target > distance:
+			position += direction * distance
+
+			var progress = 1.0 - (distance_to_target / total_distance)
+
+			if progress >= 0.33:
+				$AnimatedSprite2D.play("Middle")
+
+			if progress >= 0.66:
+				$AnimatedSprite2D.play("End")
+		else:
+			position = target_position
+			stop_moving()
+
+func shoot(target_pos: Vector2):
+	target_position = target_pos
+	moving = true
+	total_distance = global_position.distance_to(target_position)
+	$AnimatedSprite2D.look_at(target_position)
+	$AnimatedSprite2D.rotation_degrees -= 90
+	
+	$VenomPuddle.hide()  # Hide puddle initially
+	$AnimatedSprite2D.play("Start")  # Play shooting animation
+	$Hitbox/CollisionShape2D.set_deferred("disabled", true)
+
+func stop_moving():
+	moving = false
+	$Hitbox/CollisionShape2D.set_deferred("disabled", false)
+	$AnimatedSprite2D.hide()  # Hide the projectile
+	$VenomPuddle.show()  # Show puddle
+	$Timer.start(puddle_duration)  # Start puddle decay timer
+
+
+func _on_timer_timeout() -> void:
+	queue_free()  # Remove venom instance
+ # Replace with function body.
