da2bc3212409e512a99ee3012050420e10215f7c
willbhedges <willbhedges@icloud.com>
2025-04-09 14:53:43 -0500

    Got rid of the map so valhalla takes you straight to the forrest

--- viking/Map/Valhalla/home.gd
+++ viking/Map/Valhalla/home.gd
@@ -7,7 +7,7 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		#$CollisionShape2D.set_deferred("disabled", true)
 		get_parent().remove_child(body)
-		get_tree().call_deferred("change_scene_to_file", "res://Map/map.tscn")
+		get_tree().call_deferred("change_scene_to_file", "res://Map/Forest Realm/earth_tileset.tscn")
 		print("hello")
 
 
