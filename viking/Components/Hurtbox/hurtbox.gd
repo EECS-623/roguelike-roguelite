9be1c74d0223df8af77dc51e5b1079b5ea84d0f8
Chase Horner <chase.horner@ku.edu>
2025-04-14 11:37:35 -0500

    fixed typo

--- viking/Components/Hurtbox/hurtbox.gd
+++ viking/Components/Hurtbox/hurtbox.gd
@@ -3,8 +3,7 @@ extends Area2D
 class_name Hurtbox
 
 @export var health_component: HealthComponent
-@export var owner_node: CharacterBody2Dw
-
+@export var owner_node: CharacterBody2D
 signal hurt(body)
 
 # Called when the node enters the scene tree for the first time.
