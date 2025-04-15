5b5352936e13f5a71cd31674921338160a7a22fb
Andysword <andrewhuang2019@gmail.com>
2025-04-14 20:18:06 -0500

    Created states for draugr

---
+++ viking/Entities/Enemies/Draugr/draugr_state_chase.gd
@@ -0,0 +1,19 @@
+class_name DraugrStateChase extends State
+
+func enter() -> void:
+	pass
+
+# what happens when the entity exits a state
+func exit() -> void:
+	pass
+	
+# what happens during _process of the state
+func state_process(delta : float) -> State:
+	return null
+	
+func state_physics_process(delta: float) -> State:
+	return null
+
+# what happens when obtaining an input in this state
+func handle_input(_event : InputEvent) -> State:
+	return null
