1e3355bcc07ac461fdcdeeedb4cec0cdb7997599
Chase Horner <chase.horner@ku.edu>
2025-04-18 02:26:24 -0500

    actual merge done

--- viking/Game/MainMenu/main_menu.gd
+++ viking/Game/MainMenu/main_menu.gd
@@ -3,11 +3,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	#Wwise.register_game_obj(self, self.name)
-	#Wwise.register_listener(self)
-	#Wwise.load_bank_id(AK.BANKS.MUSIC)
-	#Wwise.post_event_id(AK.EVENTS.MUSIC, self)
+	Wwise.register_game_obj(self, self.name)
+	Wwise.register_listener(self)
+	Wwise.load_bank_id(AK.BANKS.MUSIC)
+	Wwise.post_event_id(AK.EVENTS.MUSIC, self)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
