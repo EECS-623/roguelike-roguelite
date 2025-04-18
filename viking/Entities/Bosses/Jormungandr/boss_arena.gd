20ce71c0c3206641d96d9ca4acd4431ca544c6e1
willbhedges <willbhedges@icloud.com>
2025-04-17 20:38:33 -0500

    remove child in boss portal

--- viking/Entities/Bosses/Jormungandr/boss_arena.gd
+++ viking/Entities/Bosses/Jormungandr/boss_arena.gd
@@ -52,6 +52,7 @@ func portal_open():
 func _on_portal_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 			#$CollisionShape2D.set_deferred("disabled", true)
+			get_parent().remove_child(body)
 			get_tree().call_deferred("change_scene_to_file", "res://Map/Valhalla/home.tscn")
 			
 
