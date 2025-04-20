7a1bb13da80582363d46b2be18222d4347e102f9
willbhedges <willbhedges@icloud.com>
2025-04-19 20:00:22 -0500

    Added Lightning attack upgrade one

--- viking/Components/SpecialAbilityComponent/cast_fireball.gd
+++ viking/Components/SpecialAbilityComponent/cast_fireball.gd
@@ -3,6 +3,8 @@ class_name CastFireball extends SpecialAbilityComponent
 @onready var player : Player = $"../.."
 var fireball = preload("res://Entities/Player/Fireball/fireball.tscn")
 
+var upgrade_level = 2 #From Will to Josh, this will be what will determine the special upgrade. Just connect a signal and a _on_signal_name_recieved to change it.
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,10 +14,40 @@ func _process(delta: float) -> void:
 	pass
 
 func cast_ability() -> void:
-	if Global.patron_god == 1:
+	if Global.patron_god == 1 and upgrade_level == 1: #base lightning
 		var new_fireball = fireball.instantiate()
 		Wwise.post_event_id(AK.EVENTS.FIREBALL, self)
 		get_tree().current_scene.add_child(new_fireball)
 		new_fireball.position = player.global_position
 		new_fireball.direction = (player.get_global_mouse_position() - player.global_position).normalized()
 		new_fireball.rotation = (player.get_global_mouse_position() - player.global_position).angle()
+	elif Global.patron_god == 1 and upgrade_level == 2: #three lightning shot.
+		var new_fireball = fireball.instantiate()
+		var mini_fireball_one = fireball.instantiate()
+		var mini_fireball_two = fireball.instantiate()
+		Wwise.post_event_id(AK.EVENTS.FIREBALL, self)
+		get_tree().current_scene.add_child(new_fireball)
+		get_tree().current_scene.add_child(mini_fireball_one)
+		get_tree().current_scene.add_child(mini_fireball_two)
+		var mouse_pos = player.get_global_mouse_position()
+		var shoot_direction = (mouse_pos - player.global_position).normalized()
+		var perpendicular = Vector2(-shoot_direction.y, shoot_direction.x)  # 90° rotated
+
+		var spacing = 20
+
+		# Main fireball (center)
+		new_fireball.position = player.global_position
+		new_fireball.direction = shoot_direction
+		new_fireball.rotation = shoot_direction.angle()
+
+		# Left mini fireball
+		mini_fireball_one.position = player.global_position + perpendicular * spacing
+		mini_fireball_one.scale = Vector2(0.5, 0.5)
+		mini_fireball_one.direction = shoot_direction
+		mini_fireball_one.rotation = shoot_direction.angle()
+
+		# Right mini fireball
+		mini_fireball_two.position = player.global_position - perpendicular * spacing
+		mini_fireball_two.scale = Vector2(0.5, 0.5)
+		mini_fireball_two.direction = shoot_direction
+		mini_fireball_two.rotation = shoot_direction.angle()
