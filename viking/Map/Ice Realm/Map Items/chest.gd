7d9cf45f364e50c24650e675da9802eceac38f9a
Evan Rogerson <142823470+9MotorGang@users.noreply.github.com>
2025-04-25 00:22:57 -0500

    Add files via upload
    added ice realm assets

---
+++ viking/Map/Ice Realm/Map Items/chest.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		$"../ButtonPrompt".visible = true
+		$"../ButtonPrompt".position = position
+		Global.has_key = true
+		Wwise.post_event_id(AK.EVENTS.KEY_ATTAIN, self)
+		print("key acquired")
