543dd8d9e6c1d94e3eb70a7516361e6573186d8a
willbhedges <willbhedges@icloud.com>
2025-04-28 21:14:32 -0500

    Optimized Scene Transition

--- viking/Game/SceneTransitionManager/scene_transition_manager.gd
+++ viking/Game/SceneTransitionManager/scene_transition_manager.gd
@@ -3,8 +3,8 @@ extends CanvasLayer
 func fade_to_scene(target: String) -> void:
 	$TextureRect.visible = true
 	#$AnimationPlayer.play('appear')
-	$AnimationPlayer.play('dissolve')
+	$AnimationPlayer.play('open')
 	await $AnimationPlayer.animation_finished
 	get_tree().call_deferred("change_scene_to_file", target)
 	$TextureRect.visible = false
-	$AnimationPlayer.play_backwards('dissolve')
+	$AnimationPlayer.play('close')
