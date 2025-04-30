da0a7874cf4d75788cfed9cb31f8dd2f3cd12655
willbhedges <willbhedges@icloud.com>
2025-04-29 21:25:09 -0500

    Fixed Ice Chest Glitch

--- viking/Map/Ice Realm/Map Items/chest.gd
+++ viking/Map/Ice Realm/Map Items/chest.gd
@@ -4,7 +4,7 @@ var chestOpened = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"../AnimatedSprite2D".play("closed")
+	$"../Sprite2D".play("closed")
 	
 
 
