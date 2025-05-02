75fec8c0c09c1753eab1b0360ca43738af069fe6
Joshua Lee <joshua.alee888@gmail.com>
2025-05-01 22:41:30 -0500

    Revert "Add files via upload"
    This reverts commit 0eb05a30a4dfb331207cef7685007bdf827b0684.

--- viking/Global/global.gd
+++ viking/Global/global.gd
@@ -2,6 +2,7 @@ extends Node
 var relics: int = 0
 var earth_enemies_left: int = 0
 var ice_enemies_left: int = 0
+var lava_enemies_left: int = 0
 var player_health: int = 100
 var mana: int = 0
 var player_speed: int = 300
@@ -9,27 +10,3 @@ var bullet_speed: int = 500
 var enemy_speed: int = 1.5
 var xp: int = 0
 var patron_god: int = 0
-var has_key: int = 0
-
-
-var loaded_banks = {}
-var world_level: int = 2
-var upgrade_level: int = 1
-
-func reset_globals():
-	relics = 0
-	earth_enemies_left = 0
-	ice_enemies_left = 0
-	player_health = 100
-	mana = 0
-	player_speed = 300
-	bullet_speed = 500
-	enemy_speed = 1.5
-	xp = 0
-	patron_god = 0
-	has_key = 0
-
-	
-	#loaded_banks = {}, need to not reset this one to not reload wwise banks.
-	world_level = 1
-	upgrade_level = 1
