1097ed33e64ea5a53ba3c7495d377cc118da2c8f
Joshua Lee <joshua.alee888@gmail.com>
2025-05-01 19:43:27 -0500

    add patron card hovering functionality and fix game over and main menu formatting

--- viking/Game/PatronSelection/patron_selection.gd
+++ viking/Game/PatronSelection/patron_selection.gd
@@ -2,7 +2,27 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Get references to card areas
+	var thor_card_area = $Thor/ThorCardArea
+	var tyr_card_area = $Tyr/TyrCardArea
+	var freya_card_area = $Freya/FreyaCardArea
+	
+	# Connect mouse enter/exit signals for Thor
+	thor_card_area.mouse_entered.connect(func(): $ThorDescription.visible = true)
+	thor_card_area.mouse_exited.connect(func(): $ThorDescription.visible = false)
+	
+	# Connect mouse enter/exit signals for Tyr
+	tyr_card_area.mouse_entered.connect(func(): $TyrDescription.visible = true)
+	tyr_card_area.mouse_exited.connect(func(): $TyrDescription.visible = false)
+	
+	# Connect mouse enter/exit signals for Freya
+	freya_card_area.mouse_entered.connect(func(): $FreyaDescription.visible = true)
+	freya_card_area.mouse_exited.connect(func(): $FreyaDescription.visible = false)
+	
+	# Ensure descriptions are hidden initially
+	$ThorDescription.visible = false
+	$TyrDescription.visible = false
+	$FreyaDescription.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,7 +36,7 @@ func _on_button_pressed() -> void:
 	SceneTransitionManager.fade_to_scene("res://Map//Valhalla/home.tscn")
 
 func _on_button_2_pressed() -> void:
-	# Tyr (Patron 2): Bonus to Max Health. This bonus is handled in home.gd since player is instantiated in Home.tscn
+	# Tyr (Patron 2): Bonus to Max Health
 	Global.patron_god = 2
 	#Wwise.post_event_id(AK.EVENTS.NEXT_DIALOUGE, self)
 	#get_tree().change_scene_to_file("res://Map//Valhalla/home.tscn")
